fff27f941542d88b3bc805c24c5ccf39be2919b8
Victor <victorwu@users.noreply.github.com>
2018-01-18 06:23:41 -0800

    edited test

--- test/test.js
+++ test/test.js
@@ -12,32 +12,29 @@ var Web3 = require("web3");
 describe('rpc_provider', function() {
 	describe('web3 rpc_provider', function() {
     it('should return -1 if rpc_provider is not found', function() {
-      // chrome.runtime.sendMessage({func: "rpc_provider"}, function(objResponse) {
-        var web3 = new Web3(new Web3.providers.HttpProvider("http://parity.kovan.whalenodes.com/"));
+      var web3 = new Web3(new Web3.providers.HttpProvider("http://parity.kovan.whalenodes.com/"));
 
-        // Get current block number of the latest sync
-        web3.eth.isSyncing(function (error, sync) {
-            
-          // stop all app activity
-          if(!error) {
+      // Get current block number of the latest sync
+      web3.eth.isSyncing(function (error, sync) {
+          
+        // stop all app activity
+        if(!error) {
 
-            // using "true" so all filters stopped but not not web3.eth.syncing polling
-            if(sync === true) {
-              web3.reset(true);
-            }
+          // using "true" so all filters stopped but not not web3.eth.syncing polling
+          if(sync === true) {
+            web3.reset(true);
+          }
 
-            // show sync info
-            else if(sync) {
-              return true;
-            }
+          // show sync info
+          else if(sync) {
+            return true;
+          }
 
-            // run the app
-            else {
-            }
-          } else return -1;
-        });
-
-      // });
+          // run the app
+          else {
+          }
+        } else return -1;
+      });
     });
 	});
 });
